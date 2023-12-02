c8c3a5c94197ed7319a1e4fe482324d65d996f56
DungNT0 <91143508+DungNT0@users.noreply.github.com>
2023-12-02 09:46:27 +0700

    update

--- threejs/main.js
+++ threejs/main.js
@@ -1,10 +1,7 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 
-// const controls = new OrbitControls(camera, renderer.domElement);
-// const loader = new GLTFLoader();
-
+const loader = new OBJLoader();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -24,13 +21,45 @@ const geometry3 = new THREE.CapsuleGeometry(1, 0.1, 5, 25);
 const material3 = new THREE.MeshBasicMaterial({ color: 0x008000 });
 const cube3 = new THREE.Mesh(geometry3, material3);
 
+
+loader.load(
+    // resource URL
+    'public/Face.obj',
+    // called when resource is loaded
+    function (object) {
+
+        scene.add(object);
+        object.position.x = -2;
+        object.position.y = -1.5;
+
+    },
+    // called when loading is in progresses
+    function (xhr) {
+
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+    },
+    // function animate(object) {
+    //     requestAnimationFrame(animate);
+    //     object.rotation.x += 0.01;
+    //     object.rotation.y += 0.01;
+    //     renderer.render(scene, camera);
+    // },
+    // called when loading has errors
+    function (error) {
+
+        console.log('An error happened');
+
+    }
+);
 scene.add(cube1);
-cube1.position.x = -4;
+cube1.position.x = -6;
 
 scene.add(cube2);
+cube2.position.x = 2;
 
 scene.add(cube3);
-cube3.position.x = 4;
+cube3.position.x = 6;
 
 scene.background = new THREE.TextureLoader().load('public/justright.jpg');
 
@@ -46,6 +75,9 @@ function animate() {
 
     cube3.rotation.x += 0.01;
     cube3.rotation.y += 0.01;
+
+    // object.rotation.x += 0.01;
+    // object.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 animate();
